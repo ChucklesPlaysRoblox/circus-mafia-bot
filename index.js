332ba699d0321ba9586fd862988c21c2e09a7f00
ChucklesPlaysRoblox <39421059+ChucklesPlaysRoblox@users.noreply.github.com>
2018-10-15 13:43:10 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const  prefix = "."
 
-   client.on("ready", () => {
+  client.on("ready", () => {
     console.log(`I have started, Don Chuckles.`); 
     client.user.setActivity(`members.`, {type:"WATCHING"});
   });
@@ -10,15 +9,7 @@ const  prefix = "."
   client.on("guildMemberAdd", function(member) {
     let role = member.guild.roles.find("name", "Associate");
     member.addRole(role).catch(console.error);
-
-     client.on('message', message => {
- 
-const command = message.content.split(" ")[0];
-command = command.slice(prefix.length);
- 
-  if (message.content === prefix + 'targets') {
-    message.reply('We have no targets right now.');
   
-  });  
+  });
 
 client.login(process.env.BOT_TOKEN);
