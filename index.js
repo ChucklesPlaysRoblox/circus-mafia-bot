e768d110572be441ea0fd6f612f5ca893e5457f7
ChucklesPlaysRoblox <39421059+ChucklesPlaysRoblox@users.noreply.github.com>
2018-10-15 12:18:28 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require('./config.json');
+
+  client.on("ready", () => {
+    console.log(`I have started, Don Chuckles.`); 
+    client.user.setActivity(`members in the Circus Mafia.`, {type:"WATCHING"});
+  });
+       
+  client.on("guildMemberAdd", function(member) {
+    let role = member.guild.roles.find("name", "Associate");
+    member.addRole(role).catch(console.error);
+});
+
+client.login(process.env.BOT_TOKEN);
